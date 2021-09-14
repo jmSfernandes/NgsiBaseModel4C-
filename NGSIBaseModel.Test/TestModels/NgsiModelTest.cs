be3b87470e212ffb955e8c1b3b49acdf695c684a
Marcelo Fernandes <jmfernandes@dei.uc.pt>
2021-09-14 15:29:30 +0100

    14/09/2021: -- added the NGSIJArray,NGSIJObject;

---
+++ NGSIBaseModel.Test/TestModels/NgsiModelTest.cs
@@ -0,0 +1,16 @@
+﻿using NGSIBaseModel.Models;
+
+namespace Tests
+{
+    public class NgsiModelTest : NgsiBaseModel
+    {
+        [NGSIIgnore] public int _id { get; set; }
+        public string id { get; set; }
+
+        [NGSIJObject] public string gpslocation { get; set; }
+
+        public string location { get; set; }
+        public double distanceHome { get; set; }
+        [NGSIJArray] public string accelerometer { get; set; }
+    }
+}
--- NGSIBaseModel/Models/NgsiBaseModel.cs
+++ NGSIBaseModel/Models/NgsiBaseModel.cs
@@ -26,6 +26,8 @@ namespace NGSIBaseModel.Models
             {
                 var isIgnore = property.GetCustomAttributes().Contains(new NGSIIgnore());
                 var isEncoded = property.GetCustomAttributes().Contains(new NGSIEncode());
+                var isJObject = property.GetCustomAttributes().Contains(new NGSIJObject());
+                var isJArray = property.GetCustomAttributes().Contains(new NGSIJArray());
 
                 JToken attribute = entity[property.Name.ToLower()];
                 if (attribute != null && !isIgnore)
@@ -33,11 +35,11 @@ namespace NGSIBaseModel.Models
                     if (attribute.Type == JTokenType.Object && attribute["value"] != null)
                     {
                         JToken value = attribute["value"];
-                        obj = SetValue<T>(obj, value, property, isEncoded);
+                        obj = SetValue<T>(obj, value, property, isEncoded, isJObject, isJArray);
                     }
                     else
                     {
-                        obj = SetValue<T>(obj, attribute, property, isEncoded);
+                        obj = SetValue<T>(obj, attribute, property, isEncoded, isJObject, isJArray);
                     }
                 }
             }
@@ -45,7 +47,8 @@ namespace NGSIBaseModel.Models
             return obj;
         }
 
-        private static T SetValue<T>(Object obj, JToken value, PropertyInfo property, bool isEncoded)
+        private static T SetValue<T>(Object obj, JToken value, PropertyInfo property, bool isEncoded, bool isJObject,
+            bool isJArray)
         {
             if (value != null)
             {
@@ -61,62 +64,73 @@ namespace NGSIBaseModel.Models
                 }
                 else if (value.Type == JTokenType.Date)
                 {
-                    DateTime dateTime= DateTime.Parse(value.ToString());
+                    DateTime dateTime = DateTime.Parse(value.ToString());
                     property.SetValue(obj, dateTime.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"));
                 }
                 else if (value.Type == JTokenType.Integer)
                 {
                     property.SetValue(obj, Int32.Parse(value.ToString()));
                 }
-                else if (value.Type == JTokenType.Float && property.PropertyType==typeof(float))
+                else if (value.Type == JTokenType.Float && property.PropertyType == typeof(float))
                 {
-                    property.SetValue(obj,float.Parse(value.ToString()));
+                    property.SetValue(obj, float.Parse(value.ToString()));
                 }
                 else if (value.Type == JTokenType.Float)
                 {
-                    property.SetValue(obj,double.Parse(value.ToString()));
+                    property.SetValue(obj, double.Parse(value.ToString()));
                 }
                 else if (value.Type == JTokenType.Array)
                 {
-                    var type = property.PropertyType;
-                    if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
+                    if (isJArray)
                     {
-                        var itemType = type.GetGenericArguments()[0]; // use this...
-                        var array = Activator.CreateInstance(type);
-                        var name = itemType.Name;
-                        foreach (JToken v in value)
+                        property.SetValue(obj, value.ToString());
+                    }
+                    else
+                    {
+                        var type = property.PropertyType;
+                        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
                         {
-                            var val = v;
-                            if (isEncoded)
-                                val = DecodeAttribute(val, property);
+                            var itemType = type.GetGenericArguments()[0]; // use this...
+                            var array = Activator.CreateInstance(type);
+                            var name = itemType.Name;
+                            foreach (JToken v in value)
+                            {
+                                var val = v;
+                                if (isEncoded)
+                                    val = DecodeAttribute(val, property);
 
-                            if (itemType.Name.Equals("String"))
-                            {
-                                array.GetType().GetMethod("Add").Invoke(array, new[] {val.ToString()});
-                            }
-                            else
-                            {
-                                Object obj_sub = Activator.CreateInstance(itemType);
-                                if (itemType.BaseType != null && itemType.BaseType == typeof(NgsiBaseModel))
+                                if (itemType.Name.Equals("String"))
                                 {
-                                    obj_sub = typeof(NgsiBaseModel).GetMethod("FromNgsi").MakeGenericMethod(itemType)
-                                        .Invoke(typeof(NgsiBaseModel), new object[] {val});
+                                    array.GetType().GetMethod("Add").Invoke(array, new[] {val.ToString()});
                                 }
                                 else
                                 {
-                                    obj_sub.GetType().GetProperty("id").SetValue(obj_sub, val.ToString());
+                                    Object obj_sub = Activator.CreateInstance(itemType);
+                                    if (itemType.BaseType != null && itemType.BaseType == typeof(NgsiBaseModel))
+                                    {
+                                        obj_sub = typeof(NgsiBaseModel).GetMethod("FromNgsi")
+                                            .MakeGenericMethod(itemType)
+                                            .Invoke(typeof(NgsiBaseModel), new object[] {val});
+                                    }
+                                    else
+                                    {
+                                        obj_sub.GetType().GetProperty("id").SetValue(obj_sub, val.ToString());
+                                    }
+
+                                    array.GetType().GetMethod("Add").Invoke(array, new object[] {obj_sub});
                                 }
-
-                                array.GetType().GetMethod("Add").Invoke(array, new object[] {obj_sub});
                             }
-                        }
 
-                        property.SetValue(obj, array);
+                            property.SetValue(obj, array);
+                        }
                     }
                 }
                 else if (value.Type == JTokenType.Object)
                 {
-                    obj = SetObjectProperty(obj, value, property, isEncoded);
+                    if (isJObject)
+                        property.SetValue(obj, value.ToString());
+                    else
+                        obj = SetObjectProperty(obj, value, property, isEncoded);
                 }
             }
 
@@ -141,34 +155,55 @@ namespace NGSIBaseModel.Models
 
         public static JObject ToNgsi<T>(Object obj)
         {
-           
-            Type mType = typeof(T);
-            JObject json = new JObject();
-
-            if (mType.GetProperty("id") != null)
+            string _id = null;
+            if (!typeof(T).Name.ToLower().Equals(obj.GetType().Name.ToLower()))
             {
-                json.Add("id",mType.GetProperty("id").GetValue(obj).ToString());
-                json.Add("type", mType.Name.ToLower());
+                throw new Exception("The type of NGSI Entity must be the same as the class");
             }
-            
 
-           
+            if (obj.GetType().GetProperty("id") == null)
+            {
+                throw new Exception(
+                    "The model is not compliant with the NGSIBaseModel it must implement an id property");
+            }
+            else
+            {
+                _id = (string) typeof(T).GetProperty("id").GetValue(obj);
+            }
+
+            JObject json = new JObject();
+            json.Add("id", _id);
+            json.Add("type", typeof(T).Name.ToLower());
+
             foreach (var property in typeof(T).GetProperties())
             {
                 var isIgnore = property.GetCustomAttributes().Contains(new NGSIIgnore());
                 var isEncoded = property.GetCustomAttributes().Contains(new NGSIEncode());
+                var isJObject = property.GetCustomAttributes().Contains(new NGSIJObject());
+                var isJArray = property.GetCustomAttributes().Contains(new NGSIJArray());
                 if (!isIgnore && (property.Name != "id"))
                 {
                     var value = obj.GetType().GetProperty(property.Name).GetValue(obj);
-                    var property_type = property.PropertyType;
+                    var propertyType = property.PropertyType;
                     if (value != null)
                     {
-                        if (property_type.IsGenericType && property_type.GetGenericTypeDefinition() == typeof(List<>))
+                        if (isJObject)
+                            value = JObject.Parse((string) value);
+                        if (isJArray)
+                            value = JArray.Parse((string) value);
+                        
+                        if (propertyType.IsClass && (isJArray || isJObject))
+                        {
+                            var attrJSON = SetJsonAttributeComplex(value, property, "object", isEncoded);
+                            json.Add(property.Name.ToLower(), attrJSON);
+                        }
+                        else if (propertyType.IsGenericType &&
+                                 propertyType.GetGenericTypeDefinition() == typeof(List<>))
                         {
                             var attrJSON = SetJsonAttributeComplex(value, property, "array", isEncoded);
                             json.Add(property.Name.ToLower(), attrJSON);
                         }
-                        else if (property_type.IsClass && !property_type.Name.ToLower().Equals("string"))
+                        else if (propertyType.IsClass && !propertyType.Name.ToLower().Equals("string"))
                         {
                             var attrJSON = SetJsonAttributeComplex(value, property, "object", isEncoded);
                             json.Add(property.Name.ToLower(), attrJSON);
@@ -184,7 +219,7 @@ namespace NGSIBaseModel.Models
 
             return json;
         }
-        
+
 
         private static JToken SetJsonAttributeSimple(Object value, PropertyInfo property, bool isEncoded)
         {
@@ -204,11 +239,11 @@ namespace NGSIBaseModel.Models
                     attrObj.Add("type", "ISO8601");
                     break;
                 case (JTokenType.Integer):
-                    attrObj.Add("value", value.ToString());
+                    attrObj.Add("value", (Int32) value);
                     attrObj.Add("type", "Integer");
                     break;
                 case (JTokenType.Float):
-                    attrObj.Add("value", value.ToString());
+                    attrObj.Add("value", (float) value);
                     attrObj.Add("type", "Float");
                     break;
                 default:
@@ -218,6 +253,7 @@ namespace NGSIBaseModel.Models
             return attrObj;
         }
 
+
         private static JToken SetJsonAttributeComplex(Object value, PropertyInfo property, String type, bool isEncoded)
         {
             JObject attrObj = new JObject();
@@ -225,7 +261,17 @@ namespace NGSIBaseModel.Models
             switch (type)
             {
                 case ("object"):
-                    if (property.GetCustomAttributes().Contains(new NGSIMapIds()))
+                    if (value.GetType() == typeof(JObject))
+                    {
+                        attrObj.Add("value", (JObject) value);
+                        attrObj.Add("type", "Text");
+                    }
+                    else if (value.GetType() == typeof(JArray))
+                    {
+                        attrObj.Add("value", (JArray) value);
+                        attrObj.Add("type", "Text");
+                    }
+                    else
                     {
                         if (value.GetType().GetProperty("id") == null)
                             throw new Exception(
@@ -233,46 +279,29 @@ namespace NGSIBaseModel.Models
                         else if (!value.GetType().GetProperty("id").PropertyType.Name.ToLower().Equals("string"))
                             throw new Exception("The id of an Object property must be a string");
                         string val = (string) value.GetType().GetProperty("id").GetValue(value);
-
                         if (isEncoded)
                             val = EncodeAttribute(val, property);
                         attrObj.Add("value", val);
-                        attrObj.Add("type", "Text");
-                    }
-                    else
-                    {
-                        attrObj.Add("value", JObject.FromObject(value));
                         attrObj.Add("type", "Text");
                     }
 
                     break;
                 case ("array"):
                     JArray array = new JArray();
-                    var typeGeneric = property.PropertyType.GetGenericArguments()[0];
-                    
-                    if (property.GetCustomAttributes().Contains(new NGSIMapIds()))
-                    {
-                        if (typeGeneric.GetProperty("id") == null)
-                            throw new Exception(
-                                "Properties of the type Objects must be a NGSIBaseModel compliant object as well");
-                        else if (!typeGeneric.GetProperty("id").PropertyType.Name.ToLower().Equals("string"))
-                            throw new Exception("The id of an Object property must be a string");
-                        foreach (var v in (IEnumerable) value)
-                        {
-                            string val_v = (string) v.GetType().GetProperty("id").GetValue(v);
-                            if (isEncoded)
-                                val_v = EncodeAttribute(val_v, property);
-                            array.Add(val_v);
-                        }
-                    }
+                    var type_generic = property.PropertyType.GetGenericArguments()[0];
+                    if (type_generic.GetProperty("id") == null)
+                        throw new Exception(
+                            "Properties of the type Objects must be a NGSIBaseModel compliant object as well");
+                    else if (!type_generic.GetProperty("id").PropertyType.Name.ToLower().Equals("string"))
+                        throw new Exception("The id of an Object property must be a string");
+
                     foreach (var v in (IEnumerable) value)
                     {
-                        if(typeGeneric.Name.ToLower().Equals("string"))
-                            array.Add(v);
-                        else
-                            array.Add( JObject.FromObject(v));
+                        string val_v = (string) v.GetType().GetProperty("id").GetValue(v);
+                        if (isEncoded)
+                            val_v = EncodeAttribute(val_v, property);
+                        array.Add(val_v);
                     }
-                   
 
                     attrObj.Add("value", array);
                     attrObj.Add("type", "Text");
@@ -300,10 +329,12 @@ namespace NGSIBaseModel.Models
 
         public override bool Equals(object obj)
         {
-            if(this.GetType() != obj.GetType())
+            if (this.GetType() != obj.GetType())
                 return false;
-            foreach (var property in this.GetType().GetProperties()) {
-                try {
+            foreach (var property in this.GetType().GetProperties())
+            {
+                try
+                {
                     var isIgnore = property.GetCustomAttributes().Contains(new NGSIIgnore());
                     if (property.PropertyType.IsGenericType &&
                         property.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
@@ -313,18 +344,18 @@ namespace NGSIBaseModel.Models
                             if (!((IEnumerable<object>) property.GetValue(obj)).Contains(value))
                                 return false;
                         }
-
                     }
-                    else if(!property.GetValue(this).Equals(property.GetValue(obj))&& !isIgnore)
+                    else if (!property.GetValue(this).Equals(property.GetValue(obj)) && !isIgnore)
                         return false;
-
-                } catch (Exception e) {
+                }
+                catch (Exception e)
+                {
                     Console.WriteLine(e.StackTrace);
                     return false;
                 }
             }
+
             return true;
-            
         }
     }
 }
