0e9b55e9a5564b79a984d4306ae42c35da791b8e
Marcelo Fernandes <jmslfernandes@gmail.com>
2022-07-01 15:24:43 +0100

    -- Fixed the license for nuget packaging; -- Fixed one of the example class in the unit tests;

--- NGSIBaseModel.Test/TestModels/SensorMapById.cs
+++ NGSIBaseModel.Test/TestModels/SensorMapById.cs
@@ -3,15 +3,14 @@ using NGSIBaseModel.Models;
 
 namespace NGSIBaseModel.Test.TestModels
 {
-    public class SensorMapById: Sensor
+    public class SensorMapById : NgsiModelObject
     {
         public string id { get; set; }
         public string model { get; set; }
-        
+
         public float accuracy { get; set; }
-        
-        [NGSIMapIds]
-        public List<Accelerometer> accelerometerList { get; set; }
+
+        [NGSIMapIds] public List<Accelerometer> accelerometerList { get; set; }
 
         public string timestamp { get; set; }
     }
