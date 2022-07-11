2e3b2d30e6417cf92dc5b223dcd9b5724188ec2a
Marcelo Fernandes <jmslfernandes@gmail.com>
2022-07-11 09:56:08 +0100

    added the Forms.Cs class example;

---
+++ NGSIBaseModel.Test/TestModels/Forms.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using NGSIBaseModel.Models;
+using NUnit.Framework;
+
+namespace NGSIBaseModel.Test.TestModels
+{
+    public abstract class Forms
+    {
+      
+
+        public class AppFinality : NgsiBaseModel
+        {
+          
+            public string id { get; set; }
+            [NGSIJObject]public string app1 { get; set; }
+            [NGSIJObject]public string app2 { get; set; }
+            [NGSIJObject]public string app3 { get; set; }
+            [NGSIJObject]public string app4 { get; set; }
+            [NGSIJObject] public string app5 { get; set; }
+            public string timestamp { get; set; }
+           
+
+        }
+      
+        
+    }
+}
